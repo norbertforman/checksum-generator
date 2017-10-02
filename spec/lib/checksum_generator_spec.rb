07f5f591b019f06feb5db4dbeda95c80512cdc22
Norbert Forman <norbert.forman@verycreatives.com>
2017-10-02 10:51:59 +0300

    added the tests that need to be passed

--- spec/lib/checksum_generator_spec.rb
+++ spec/lib/checksum_generator_spec.rb
@@ -2,10 +2,40 @@ require 'spec_helper'
 require 'checksum_generator'
 
 describe ChecksumGenerator do
-  describe '.generate' do
-    cg = ChecksumGenerator.new('').generate
-    it 'empty hash' do
-      expect(cg).to eq({})
+  describe '.generate 1st string' do
+    cg = ChecksumGenerator.new('foo bar baz wibble fizzbuzz fizz buzz').generate
+    it 'original words' do
+      expect(cg[:number_of][:original_words]).to eq(7)
+    end
+    it 'original characters' do
+      expect(cg[:number_of][:original_characters]).to eq(37)
+    end
+    it 'newly created words' do
+      expect(cg[:number_of][:newly_created_words]).to eq(4)
+    end
+    it 'upper case vowels' do
+      expect(cg[:number_of][:upper_case_vowels]).to eq(5)
+    end
+    it 'consonants' do
+      expect(cg[:number_of][:upper_case_vowels]).to eq(21)
+    end
+  end
+  describe '.generate 2nd string' do
+    cg = ChecksumGenerator.new('The quick brown fox jumps over the lazy dog').generate
+    it 'original words' do
+      expect(cg[:number_of][:original_words]).to eq(9)
+    end
+    it 'original characters' do
+      expect(cg[:number_of][:original_characters]).to eq(43)
+    end
+    it 'newly created words' do
+      expect(cg[:number_of][:newly_created_words]).to eq(4)
+    end
+    it 'upper case vowels' do
+      expect(cg[:number_of][:upper_case_vowels]).to eq(3)
+    end
+    it 'consonants' do
+      expect(cg[:number_of][:upper_case_vowels]).to eq(24)
     end
   end
 end
